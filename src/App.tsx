3449191f5df62f94d7ebf4ea6ea878ce86e2251c
Keisuke Taniguchi <rememberthatk@icloud.com>
2021-01-16 08:25:27 +0900

    [master] add Bezier sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import BezierEditor from 'bezier-easing-editor'
+import React, { useState } from 'react'
+
+// type CubicBezier = {
+//   x1: number
+//   x2: num
+// }
 
 function App() {
+  const [value, setValue] = useState<number[]>([0, 0, 1, 1])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bezier</h1>
+      <BezierEditor
+        className="bezier"
+        value={value}
+        onChange={setValue}
+        handleStroke={3}
+        handleRadius={6}
+        curveWidth={3}
+      >
+        <text x={0} y={16} fill="#f00">
+          Controlled Bezier Editor
+        </text>
+      </BezierEditor>
+      <p>{value.join(' ')}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

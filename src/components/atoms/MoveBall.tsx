876a518771bbfba1e0f167b2bf0006f655bd8c7b
Keisuke Taniguchi <rememberthatk@icloud.com>
2021-01-16 15:32:39 +0900

    [master] add shadow params to MoveBall

--- src/components/atoms/MoveBall.tsx
+++ src/components/atoms/MoveBall.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 interface MoveBallProps {
   position: number
+  shadow?: boolean
 }
 
-export const MoveBall = ({ position }: MoveBallProps) => {
+export const MoveBall = ({ position, shadow = false }: MoveBallProps) => {
   return (
     <div
-      className="absolute w-10 h-10 rounded-full bg-red-400"
+      className={`absolute w-10 h-10 rounded-full bg-red-400 ${
+        shadow && 'bg-opacity-30'
+      }`}
       style={{ left: position }}
     ></div>
   )
